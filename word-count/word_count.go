e144150bbe078b81a2d53cb5925b5b9981c1ee3b
sarahbaranski <sbaranski@gmail.com>
2022-07-13 21:01:34 -0500

    passed word_count

--- word-count/word_count.go
+++ word-count/word_count.go
@@ -6,6 +6,8 @@ import (
 
 type Frequency map[string]int
 
+const singleQuote = rune('\'')
+
 func WordCount(fullInput string) Frequency {
 	freqMap := Frequency{}
 	lower := strings.ToLower(fullInput)
@@ -13,8 +15,15 @@ func WordCount(fullInput string) Frequency {
 	noPeriods := strings.Replace(noColons, ".", " ", -1)
 	noSpecChar := strings.Replace(noPeriods, "!!&@$%^&", " ", -1)
 	wordSlice := strings.Fields(strings.Replace(noSpecChar, ",", " ", -1))
-	for _, currentWord := range wordSlice {
-		freqMap[currentWord]++
+	for _, w := range wordSlice {
+		if strings.ContainsRune(w, singleQuote) && !IsContraction(w) {
+			w = strings.ReplaceAll(w, string(singleQuote), "")
+		}
+		freqMap[w]++
 	}
 	return freqMap
+}
+func IsContraction(fullInput string) bool {
+	index := strings.IndexRune(fullInput, singleQuote)
+	return index+1 < len(fullInput) && index-1 > 0
 }
