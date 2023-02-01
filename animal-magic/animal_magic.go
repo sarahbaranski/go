f0ac988c05e19115b0cf36251401121dc088ca42
sarahbaranski <sbaranski@gmail.com>
2023-02-01 11:38:37 -0600

    passed animal-magic

---
+++ animal-magic/animal_magic.go
@@ -0,0 +1,32 @@
+package chance
+
+import (
+	"math/rand"
+	"time"
+)
+
+// SeedWithTime seeds math/rand with the current computer time.
+func SeedWithTime() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// RollADie returns a random int d with 1 <= d <= 20.
+func RollADie() int {
+	return rand.Intn(20) + 1
+}
+
+// GenerateWandEnergy returns a random float64 f with 0.0 <= f < 12.0.
+func GenerateWandEnergy() float64 {
+	return rand.Float64() * 12
+}
+
+// ShuffleAnimals returns a slice with all eight animal strings in random order.
+func ShuffleAnimals() []string {
+	animal := []string{"ant", "beaver", "cat", "dog", "elephant", "fox", "giraffe", "hedgehog"}
+
+	rand.Shuffle(len(animal), func(i, j int) {
+		animal[i], animal[j] = animal[j], animal[i]
+	})
+
+	return animal
+}
